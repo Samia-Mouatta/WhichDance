550b89113097324c304db0a94c4e6fdf75c4529a
Samia MOUATTA <samia.mouatta@etu.univ-nantes.fr>
2018-10-13 20:48:01 +0200

    Save imageView in internal storage

--- app/src/main/java/mouatta/samia/whichdance/BalletActivity.java
+++ app/src/main/java/mouatta/samia/whichdance/BalletActivity.java
@@ -1,15 +1,19 @@
 package mouatta.samia.whichdance;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,10 +22,16 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by samia on 02/10/2017.
@@ -29,17 +39,18 @@ import java.io.IOException;
 
 public class BalletActivity extends BaseActivity {
 
-
+    Button btn;
+    GridView gridView;
+    ImageView imageView;
     public int[] idImg = {R.drawable.ballerine, R.drawable.pointesrose, R.drawable.pointesrouge, R.drawable.pointesrose, R.drawable.pointesrouge, R.drawable.ballerine};
-    //0,1,2,3,4,5
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gridview);
 
-       // Button btn = (Button) findViewById(R.id.btn);
-        GridView gridView = (GridView) findViewById(R.id.ballet_grid_view);
+        gridView = (GridView) findViewById(R.id.ballet_grid_view);
 
         // Instance of ImageAdapter Class
         gridView.setAdapter(new ImageAdapter(BalletActivity.this,idImg));
@@ -50,79 +61,27 @@ public class BalletActivity extends BaseActivity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
+                                    final int position, long id) {
 
-                //
-                // Sending image id to FullScreenActivity
-               // Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
-                // passing array index
-                /*i.putExtra("id", position);
-                startActivity(i);*/
                 setContentView(R.layout.full_image);
 
-                ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
-                imageView.setImageResource(new ImageAdapter(BalletActivity.this).idImg[position]);
-/*                BitmapDrawable draw = (BitmapDrawable) imageView.getDrawable();
-                Bitmap bitmap = draw.getBitmap();
+                imageView = (ImageView) findViewById(R.id.full_image_view);
+                imageView.setImageResource(idImg[position]);
 
-                FileOutputStream outStream = null;
-             //   File sdCard = Environment.getExternalStorageDirectory();
-              //  File dir = new File(sdCard.getAbsolutePath() + "/YourFolderName");
-                String tempFilePath = Environment.getExternalStorageDirectory() + "/"
-                        + getPackageName() + "/" + "toto" + ".jpg";
-               // dir.mkdirs();
-               // String fileName = String.format("%d.jpg", System.currentTimeMillis());
-                File outFile = new File(tempFilePath);
-                try {
-                    outStream = new FileOutputStream(outFile);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-                try {
-                    outStream.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    outStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                intent.setData(Uri.fromFile(outFile));
-                sendBroadcast(intent);*/
-                //Toast.makeText(BalletActivity.this, "You Clicked at " +idImg[+ position], Toast.LENGTH_SHORT).show();
+                btn = (Button) findViewById(R.id.btn);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            saveImage();
+                    }
+                });
             }
         });
 
-       /* btn.setOnClickListener(new View.OnClickListener(){
-
-                                   @Override
-                                   public void onClick(View v) {
-                                       Drawable drawable = getResources().getDrawable(R.drawable.ballerine);
-                                       Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-                                       // Save image to gallery
-                                       String savedImageURL = MediaStore.Images.Media.insertImage(
-                                               getContentResolver(),
-                                               bitmap,
-                                               "Bird",
-                                               "Image of bird"
-                                       );
-
-                                       // Parse the gallery image url to uri
-                                       Uri savedImageURI = Uri.parse(savedImageURL);
-
-                                       // Display saved image url to TextView
-                                       Toast.makeText(BalletActivity.this,"Image saved to gallery."+savedImageURI, Toast.LENGTH_SHORT).show();
-                                   }
-        });*/
         // back button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -133,6 +92,59 @@ public class BalletActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //save imageView
+    public void saveImage(){
+        FileOutputStream fileOutputStream = null;
+        File file = getDirc();
+
+        // verifier si le fichier ou dossier existe
+        if(!file.exists() && !file.mkdirs()){
+            Toast.makeText(this,"Impossible de créer un répertoire",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+        String date = simpleDateFormat.format(new Date());
+        String photofile = "Img" + date + ".jpg";
+        String file_name = file.getPath() + "/" + photofile;
+        File picfile = new File(file_name);
+        try {
+            fileOutputStream = new FileOutputStream(picfile);
+            Bitmap bitmap = convertViewToBitmap(imageView,imageView.getWidth(),imageView.getHeight());
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,fileOutputStream);
+            Toast.makeText(getApplicationContext(), "Sauvegarde réussie", Toast.LENGTH_SHORT).show();
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        MajGallery(picfile);
+    }
+
+    //convert imageview to bitmap
+    public static Bitmap convertViewToBitmap(View view, int width, int height){
+        Bitmap bitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
+    }
+
+    //update gallery
+    public void MajGallery(File file) {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(Uri.fromFile(file));
+        sendBroadcast(intent);
+    }
+
+    //create a new folder in DCIM Directory
+    public File getDirc() {
+        File dics = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        return new File(dics, " Which_Dance_Img");
     }
 
 }
