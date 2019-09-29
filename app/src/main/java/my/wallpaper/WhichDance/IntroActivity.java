7d400f1c3a64af2ea8ca2adeb582a3fe63759f53
Samia MOUATTA <samia.mouatta@etu.univ-nantes.fr>
2019-09-29 17:28:58 +0200

    actionBar color transparent

--- app/src/main/java/my/wallpaper/WhichDance/IntroActivity.java
+++ app/src/main/java/my/wallpaper/WhichDance/IntroActivity.java
@@ -1,8 +1,9 @@
-package my.app.WhichDance;
+package my.wallpaper.WhichDance;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
@@ -45,6 +46,7 @@ public class IntroActivity extends AppCompatActivity {
         // Making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         }
 
         setContentView(R.layout.activity_intro);
